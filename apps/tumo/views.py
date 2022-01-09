42b8331f5f78d742a373a000ae319253f431ad54
cauecjm <cauecjm@gmail.com>
2022-01-09 20:53:51 -0300

    criando estrutura do site

--- apps/tumo/views.py
+++ apps/tumo/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
 
+from apps.tumo.utils import base_context
+
+
 def home(request):
-    return render(request, 'tumo/index.html')
+    return render(request, 'tumo/pages/index.html', {**base_context('tumo-home')})
