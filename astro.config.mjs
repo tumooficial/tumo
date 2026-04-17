44b9400fd1099786fd1f18ec207eac8ab839841f
Cauê J. Martinez <cauecjm@gmail.com>
2026-04-17 08:56:37 -0300

    Add config output 'server' no astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://www.tumo.com.br'
+  site: 'https://www.tumo.com.br',
+  output: 'server',
+  adapter: vercel(),
 });
